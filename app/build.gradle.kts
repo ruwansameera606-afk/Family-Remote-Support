454dc930aa4467959a49b7a14be48087607fbfd6
ruwansameera606-afk <ruwansameera606@gmail.com>
2026-08-16 19:56:08 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.familyremotesupport"
+    namespace = "com.family.remotesupport"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.familyremotesupport"
+        applicationId = "com.family.remotesupport"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
